c7fd3bc44d4d48d86d5f3ec76c206fabdc86ee22
angelm1974 <mariusz980@gmail.com>
2023-11-24 14:26:07 +0100

    mongo1

--- obsluga_mongo.py
+++ obsluga_mongo.py
@@ -2,3 +2,28 @@ import pymongo as pm
 
 serwer_adres="mongodb+srv://student:HOdg80lm7WqGGG92@cluster0.cddpk.mongodb.net/?retryWrites=true&w=majority"
 baza_danych="pierwsza_baza"
+kolekcja="kolarze"
+
+serwer=pm.MongoClient(serwer_adres)
+baza=serwer.get_database(baza_danych)
+
+def dodaj_kolarza(dane):
+    kolekcja_kolarze=baza.get_collection("kolarze")
+    kolekcja_kolarze.insert_one(dane)
+
+def pobierz_kolarzy():
+    kolekcja_kolarze=baza.get_collection("kolarze")
+    kolarze=kolekcja_kolarze.find()
+    return kolarze
+
+
+kolarz={
+    "imie":"Jan",
+    "nazwisko":"Kowalski",
+    "waga":80,
+}
+
+kolarze=pobierz_kolarzy()
+for kolarz in kolarze:
+    print(kolarz)
+# dodaj_kolarza(kolarz)
