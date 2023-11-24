885fff844949c4164c8d4fc9e328ccbff5b51fc0
angelm1974 <mariusz980@gmail.com>
2023-11-24 14:43:49 +0100

    mongo3

--- obsluga_mongo.py
+++ obsluga_mongo.py
@@ -16,14 +16,32 @@ def pobierz_kolarzy():
     kolarze=kolekcja_kolarze.find()
     return kolarze
 
+def update_kolarza(kolarz):
+    kolekcja_kolarze=baza.get_collection("kolarze")
+    kolekcja_kolarze.update_one({"_id":kolarz.get("_id")}, {"$set":kolarz})
+
+def wyszukaj_po_nr_startowym(nr_startowy):
+    kolekcja_kolarze=baza.get_collection("kolarze")
+    kolarz=kolekcja_kolarze.find_one({"numer_startowy":nr_startowy})
+    return kolarz
+
+# def dodaj_numer_startowy_inkrementacyjnie():
+#     nr=1
+#     kolekcja_kolarze=baza.get_collection("kolarze")
+#     kolarze=kolekcja_kolarze.find()
+#     for kolarz in kolarze:
+#         kolarz["numer_startowy"]=nr
+#         nr += 1
+#         update_kolarza(kolarz)
 
 kolarz={
     "imie":"Jan",
     "nazwisko":"Kowalski",
     "waga":80,
 }
-
-kolarze=pobierz_kolarzy()
-for kolarz in kolarze:
-    print(kolarz)
+# dodaj_numer_startowy_inkrementacyjnie()
+# kolarze=pobierz_kolarzy()
+# for kolarz in kolarze:
+#     print("parametry: ",kolarz.get("imię"), kolarz.get("nazwisko"), kolarz.get("waga")  )
 # dodaj_kolarza(kolarz)
+print(wyszukaj_po_nr_startowym(1))
